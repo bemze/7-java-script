0f5d9e43a5a35d432305cfa32f940e9c387fb30f
Mantas <bemze007@yahoo.com>
2020-11-11 13:44:42 +0200

    js intro

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// spausdiname sveikaji skaiciu
+console.log(88888);
+
+// 2 skaiciai vs desimtainis - kaip ir koks kablelis
+console.log(3 , 14)
+console.log(3.14)
+
+// kaip spausdinamas tekstas
+console.log ("Labas rytas");
+console.log ("1984-04-05");
