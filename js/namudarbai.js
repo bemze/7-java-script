01dd605cd03d9dc3b4b8cee1cf742bd8748ff796
Mantas <bemze007@yahoo.com>
2020-11-12 09:38:56 +0200

    namu darbai

---
+++ js/namudarbai.js
@@ -0,0 +1,60 @@
+
+// NAMU DARBAI 2020-11-11
+
+// Kintamųjų inicijavimas
+
+// 1 uzduotis 
+// Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
+
+
+const vienas = 1
+console.log(vienas);
+
+const du = 15
+console.log(du);
+
+const trys = 59
+console.log(trys);
+
+// 2 uzduotis
+// Sukurti 3 kintamuosius su teksto tipo reikšmėmis
+
+
+const as ="Mantas"
+console.log(as);
+
+const tu ="Tomas"
+console.log(tu);
+
+const mes = "visi zmones"
+console.log(mes);
+
+// 3 uzduotis
+// Sukurti 3 sąrašo tipo kintamuosius su penkiomis skaičių tipo reikšmėmis
+
+const taskai = [15, 20, 4, 9, 19]
+console.log (taskai)
+
+const nuostoliai = [-150, -20, -400, 0, -55]
+console.log (nuostoliai)
+
+const pelnas = [500, 20, 425, 0, 355]
+console.log (nuostoliai)
+
+
+// 4 uzduotis
+// Sukurti 3 sąrašo tipo kintamuosius su penkiomis teksto tipo reikšmėmis
+
+const komandos = ["Juventus", "Rytas", "Zalgiris", "Neptunas", "Lietkabelis"]
+console.log (komandos)
+
+const firmos = ["Nike", "Adidas", "Puma", "Gucci", "No-name"]
+console.log (firmos)
+
+const bankai = ["Swedbank", "SEB", "Luminor", "Danske", "Snoras"]
+console.log (bankai)
+
+// Veiksmai su kintamaisiais
+// 1 Susumuoti visus skaičiaus tipo kintamuosius
+
+
