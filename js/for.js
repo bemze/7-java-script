f3aa4ec9f1807b77e0b3916a869855541bcff4ad
Mantas <bemze007@yahoo.com>
2020-11-17 15:47:44 +0200

    praktika3

--- js/for.js
+++ js/for.js
@@ -12,5 +12,112 @@ const zodis = "abcdef";
 for (let i = zodis.length - 1; i >= 0; i=i) {
     console.log(zodis.length);
 }
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+console.log('------------');
+
+/*
+Jei kas menesi zmogus uzdirba 5000, tai kiek jis tures pajamu per metus?
+*/
+
+const alga = 5000;
+const menesiu = 12;
+let pajamos = 0;
+
+for (let i = 1; i <= menesiu; i++) {
+    pajamos = pajamos + alga;
+    console.log(`${i}) ${pajamos} pinigu.`);
+}
+
+console.log('-----------');
+
+/*
+Parasyti skaicius nuo 8 iki 4 imtinai.
+*/
+
+for (let i = 8; i >= 4; i--) {
+    console.log(i);
+}
+
+console.log('-----------');
+
+/*
+Isvardinti ciferblato skaicius nuo duotos valandos iki galo.
+*/
+
+const valanda = 10;
+const maxValandu = 12;
+
+for (let val = valanda; val <= maxValandu; val++) {
+    console.log(val + ' val');
+}
+
+console.log('------------');
+
+/*
+Duotas miestu sarasas. Isvardinti kiekviena aplankyta miesta.
+Spausdiname: "Aplankiau: Miesto pavadinimas."
+*/
+
+const miestai = ['Vilnius', 'Kaunas', 'Klaipeda', 'Anyksciai'];
+
+// console.log(`Aplankiau: ${miestai[0]}.`);
+// console.log(`Aplankiau: ${miestai[1]}.`);
+// console.log(`Aplankiau: ${miestai[2]}.`);
+// console.log(`Aplankiau: ${miestai[3]}.`);
+
+for (let i = 0; i < miestai.length; i++) {
+    const miestoPavadinimas = miestai[i];
+    console.log(`Aplankiau: ${miestoPavadinimas}.`);
+}
+
+console.log('------------');
+
+/*
+Isvardinti saraso narius is galo.
+*/
+
+const abc = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+for (let i = abc.length - 1; i >= 0; i--) {
+    console.log(abc[i]);
+}
+
+console.log('-------------');
+console.clear();
+
+// const text = 'dodekahedronas';
+const text = 'ottoman';
+
+for (let i = 0; i < text.length; i++) {
+    console.log(text[i]);
+}
+
+console.log(text.length);
+
+console.log('-------------');
+
+/*
+Duodamas zodis ir is jo reikia isrinkti kas antra raide ir isspausdinti kas gaunasi.
+pvz.: abcdef -> bdf
+pvz.: pomidoras (2) -> oioa
+pvz.: pomidoras (3) -> mos
+pvz.: pomidoras (4) -> ia
+pvz.: pomidoras (5) -> d
+*/
+
+const zodis = 'pomidoras';
+const step = 2;
+const pirmosRaidesPozicija = step - 1;
+let rez = '';
+
+for (let i = pirmosRaidesPozicija; i < zodis.length; i = i + step) {
+    const raide = zodis[i];
+    rez = rez + raide;
+}
+
+console.log(rez);
 
 
---
+++ js/praktika2.js
@@ -0,0 +1,269 @@
+
+for (let i = 0; i <= 10; i++){
+    console.log(i)
+}
+const skaicius = 15
+if (skaicius > 5){
+    console.log("ZJBS")};
+     if (skaicius < 5)
+    {console.log("Nepaejo")}
+    else {console.log("Kas toliau")};
+
+// kaip suskaiciuoti pazymiu vidurki?
+
+const marks = [10, 8, 7, 9, 2, 10];
+console.log(marks);
+
+let suma2 = 0;
+suma2 = suma2 + (marks[0]);
+suma2 = suma2 + (marks[1]);
+suma2 = suma2 + (marks[2]); 
+suma2 = suma2 + (marks[3]);
+suma2 = suma2 + (marks[4]);
+suma2 = suma2 + (marks[5]);
+const kiekis2 = marks.length;
+const vidurkis2 = suma2 / kiekis2;
+console.log(vidurkis2);
+console.log(marks.length)
+console.log(suma2)
+
+
+// const petriukoPazymiai = [2, 10, 7, 5];
+// console.log(petriukoPazymiai);
+
+// // kaip suskaiciuoti pazymiu vidurki?
+// let suma = 0;
+// suma = suma + petriukoPazymiai[0];
+// suma = suma + petriukoPazymiai[1];
+// suma = suma + petriukoPazymiai[2];
+// suma = suma + petriukoPazymiai[3];
+// // suma = suma + petriukoPazymiai[4];
+
+// const kiekis = petriukoPazymiai.length;
+// const vidurkis = suma / kiekis;
+
+// console.log(vidurkis);
+
+// Edita Zabiele 35 metu
+
+const vardas = "Edita";
+const pavarde = "Zabiele";
+const amzius = 1985;
+
+const posakis = (`${vardas} ${pavarde} yra (${2020 - amzius}) metu.`);
+console.log(posakis);
+
+
+let turiu = 1111;
+const sausis = 100;
+const vasaris = 200;
+
+turiu = turiu + sausis;
+// const viso = turiu + vasaris;
+console.log(turiu);
+
+turiu = turiu + vasaris;
+console.log(turiu);
+ 
+
+const miestai = ['Vilnius', 'Kaunas', 'Klaipeda', 'Siauliai'];
+console.log(miestai);
+
+for (let i = miestai.length - 1; i >= 0; i--){
+    console.log(miestai[i]);
+}
+
+const zodis = "besikiskiapopusdeliaudamasis"
+for (let i = 0; i < zodis.length; i=i+5){
+    console.log(zodis[i]);
+}
+
+const zod = "braskyte";
+for (let i = 0; i < zod.length; i++){
+    console.log(zod[i]);
+}
+
+console.clear();
+
+
+const sk = 8;
+console.log(sk);
+
+const zo1 = "Begti";
+console.log(zo1);
+
+const eile = [10, 15, 16, 17, 20];
+let sum = 0;
+sum = sum + eile[0];
+sum = sum + eile[1];
+sum = sum + eile[2];
+sum = sum + eile[3];
+sum = sum + eile[4];
+console.log(sum);
+
+
+const zo2 = ["begti", "eiti", "ciuozti"];
+console.log(zo2);
+console.log(zo2[0]);
+let visiZo2 = "";
+visiZo2 = visiZo2 + zo2[0];
+visiZo2 = visiZo2 + ', ' + zo2[1];
+visiZo2 = visiZo2 + ', ' + zo2[2];
+console.log(visiZo2)
+console.log(`begti, eiti, ciuozti`);
+
+// logika 10-15+16-17+20
+
+let i = 0;
+i = i + eile[0];
+i = i - eile[1];
+i = i + eile[2];
+i = i - eile[3];
+i = i + eile[4];
+console.log(i);
+
+let sar = "";
+sar = sar + zo2[2];
+sar = sar + ", " + zo2[1];
+sar = sar + ', ' + zo2[0];
+console.log(sar);
+
+
+const no = 10;
+const no1 = 11;
+
+if (no >= no1){
+    console.log("Pomidoras");
+}
+if (no < no1){
+    console.log("Bandykite dar karta");
+}
+
+if (no < no1){
+    console.log(true);
+}
+if (no > no1){
+    console.log(false);
+}
+if (no === no1){
+    console.log("equal");
+}
+if (no !== no1){
+    console.log("Nelygus");
+}
+
+// const abc = "Braskyte";
+// const abcd = "Braskyte"
+// console.log(abc.length);
+// console.log(abcd.length);
+
+// if (abc.length > abcd.length){
+//     console.log("Taip ir norejau");
+// }
+
+//    else if (abc.length === abcd.length){
+//         console.log("Paieskok ilgesnio zodzio");
+//     }
+//     else {console.log("radai lygybe");
+// }
+
+const position = "Misi";
+if (position === "Suniukas"){
+    console.log("Keisk poza");
+} else if ( position === "Doogyy"){
+    console.log("Neblogai");
+} else {console.log("Nemoki p****, nereikia list");
+}
+
+
+const paz = [10, 5, 10];
+let sum1 = 0;
+sum1 = sum1 + paz[0];
+sum1 = sum1 + paz[1];
+sum1 = sum1 + paz[2];
+console.log(sum1);
+console.log(sum1 / paz.length);
+
+const aa ="Arkassssssss";
+const bb = "Karves";
+
+console.log(aa.length);
+
+if (aa.length > bb.length){
+    console.log(true);}
+    // console.log(false);}
+
+
+// } else if (aa.length < bb.length){
+//     console.log(false);
+// } else {console.log("lygu")}
+
+// if (aa.length > bb.length){
+//     console.log(true)
+// } 
+// if (aa.length >= bb.length){
+//     console.log(true)
+// } 
+// if (aa.length <= bb.length){
+//     console.log(true)
+// } 
+
+
+
+// if (aa.length > bb.length){
+//     console.log(true)
+// } 
+
+// if (aa.length < bb.length){
+//     console.log(false);
+// } 
+
+// if(aa.length == bb.length) {
+//     console.log("lygu")
+// }
+// if(aa.length != bb.length) {
+//     console.log("lygu")
+// }
+// if(!aa.length == bb.length) {
+//     console.log("lygu")
+// }
+
+
+const zzz = [8, 10, 10, 15, 20, 20, 1, 10];
+const xxx = [ 1, 2, 4, 10, 5, 6, 10];
+console.log(zzz.length);
+console.log(xxx.length);
+
+if (zzz.length > xxx.length){
+    console.log("Didesnis");
+} else if (zzz.length < xxx.length){
+    console.log('Mazesnis');
+} else if (zzz.length === xxx.length){
+    console.log('Lygu');
+}
+
+const k = 4;
+// let count = 0;
+for (let i =0; i < k; i++){
+    console.log(k, i);
+}
+
+for (let i = 0; i <= 4; i++){
+    console.log(i);
+}
+
+const alga = 1212;
+const men = 12;
+let pajamos = 0;
+
+for (let i = 1; i <= men; i++){
+    pajamos = pajamos + alga;
+    console.log(`${i}) ${pajamos}`);
+}
+
+const w = "Lietuva";
+let back = "";
+for (i = 0; i < w.length; i++){
+    const letter = i; 
+}
+console.log(text, '->', back);
